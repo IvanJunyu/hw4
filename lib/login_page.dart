384c1a96ffaace6eef1117e3628ef9e6bd19d899
Ivan Loh <ivanjloh2020@gmail.com>
2024-11-17 22:34:24 -0500

    fix another bug

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'home_page.dart';
+import 'registration_page.dart';
 
 class LoginPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
@@ -30,19 +31,32 @@ class LoginPage extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
               controller: emailController,
               decoration: InputDecoration(labelText: "Email"),
             ),
+            SizedBox(height: 10),
             TextField(
               controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(labelText: "Password"),
             ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => loginUser(context),
               child: Text("Login"),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegistrationPage()),
+                );
+              },
+              child: Text("Don't have an account? Register Here"),
             ),
           ],
         ),
